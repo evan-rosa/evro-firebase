d9dfe2bd0b5246b187479811a494b14b3c11d5ce
Evan Rosa <evanrosa@Evans-MacBook-Pro-2.local>
2020-09-02 22:20:50 -0400

    gtag

--- src/gtag.js
+++ src/gtag.js
@@ -1,49 +1,50 @@
-import * as React from "react";
+import * as React from 'react';
 
-const GA_TRACKING_ID = "GTM-P4DJ4N2";
+const GA_TRACKING_ID = 'GTM-P4DJ4N2';
+const GA_OPTIMIZE_ID = 'OPT-5K72PSC';
+
 export const GtagScript = () => {
-    function intercept() {
-        const qs = document.querySelector("#gtm-js");
-        if (qs !== null) {
-            qs.addEventListener("load", () => {
-            });
-        }
+  function intercept() {
+    const qs = document.querySelector('#gtm-js');
+    if (qs !== null) {
+      qs.addEventListener('load', () => {});
     }
+  }
 
-    return (
-        <>
-            <script
-                id="gtm-js"
-                async
-                src={`https://www.googletagmanager.com/gtm.js?id=${GA_TRACKING_ID}`}
-            />
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
+  return (
+    <>
+      <script
+        id='gtm-js'
+        async
+        src={`https://www.googletagmanager.com/gtm.js?id=${GA_TRACKING_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
+            gtag('config', '${GA_TRACKING_ID}',{ 'optimize_id': '${GA_OPTIMIZE_ID}'} );
             (${intercept.toString()})()
-            `
-                }}
-            />
-        </>
-    );
+            `,
+        }}
+      />
+    </>
+  );
 };
 const style = {
-    display: "none",
-    visibility: "hidden"
+  display: 'none',
+  visibility: 'hidden',
 };
-export const GtagNoscript = props => (
-    <>
-        <noscript>
-            <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
-                height="0"
-                width="0"
-                style={style}
-            />
-        </noscript>
-    </>
+export const GtagNoscript = (props) => (
+  <>
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
+        height='0'
+        width='0'
+        style={style}
+      />
+    </noscript>
+  </>
 );
