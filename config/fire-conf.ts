1232a878c74b7f575804b464953931cac43de045
Evan Rosa <erosa26@gmail.com>
2020-09-24 12:30:29 -0400

    sandbox v live

--- config/fire-conf.ts
+++ config/fire-conf.ts
@@ -3,7 +3,8 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-const firebaseConfig = {
+//Sandbox
+const firebaseConfigSandbox = {
   apiKey: process.env.FB_API_KEY,
   authDomain: process.env.FB_AUTH_DOMAIN,
   databaseURL: process.env.FB_DB_URL,
@@ -13,10 +14,26 @@ const firebaseConfig = {
   appId: process.env.FB_APP_ID,
   measurementId: process.env.FB_MEASUREMENT_ID,
 };
+//Live
+const firebaseConfigLive = {
+  apiKey: process.env.FB_API_KEY_LIVE,
+  authDomain: process.env.FB_AUTH_DOMAIN_LIVE,
+  databaseURL: process.env.FB_DB_URL_LIVE,
+  projectId: 'evro-live-backend',
+  storageBucket: process.env.FB_STORAGE_BUCKET_LIVE,
+  messagingSenderId: process.env.FB_MESSAGING_SENDING_ID_LIVE,
+  appId: process.env.FB_APP_ID_LIVE,
+  measurementId: process.env.FB_MEASUREMENT_ID_LIVE,
+};
 // Initialize Firebase
 try {
-  firebase.initializeApp(firebaseConfig);
-  firebase.storage();
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    firebase.initializeApp(firebaseConfigSandbox);
+    firebase.storage();
+  } else {
+    firebase.initializeApp(firebaseConfigLive);
+    firebase.storage();
+  }
 } catch (err) {
   if (!/already exists/.test(err.message)) {
     console.error('Firebase initialization error', err.stack);
