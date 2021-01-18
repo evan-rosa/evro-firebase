a6db17635bfd8ac39d8bcbf112500c655d6bc7cf
Evan Rosa <erosa26@gmail.com>
2021-01-18 11:37:56 -0500

    new gtm flow - revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,15 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import React from 'react';
+import App from 'next/app';
 
-import Router from 'next/router';
-import { GTMPageView } from '../config/gtm';
-import { useEffect } from 'react';
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
 
-
-import { AppProps } from 'next/app';
-
-
-
-function MyApp({ Component, pageProps }: AppProps) {
-
-
-  // Initiate GTM
-  useEffect(() => {
-      const handleRouteChange = (url: string) => GTMPageView(url);
-      Router.events.on('routeChangeComplete', handleRouteChange);
-      return () => {
-          Router.events.off('routeChangeComplete', handleRouteChange);
-      };
-  }, []);
-
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import * as React from 'react';
 import flush from 'styled-jsx/server';
+import { GtagNoscript, GtagScript } from '../src/gtag';
 
 class MyDocument extends Document {
   render() {
@@ -10,26 +11,10 @@ class MyDocument extends Document {
       <Html lang='en' dir='ltr'>
         <Head>
           <meta charSet='utf-8' />
-          {/* Google Tag Manager */}
-          <script
-              dangerouslySetInnerHTML={{
-                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
-              }}
-          />
-          {/* End Google Tag Manager */}
+          <GtagScript />
         </Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript
-              dangerouslySetInnerHTML={{
-                  __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-              }}
-          />
-          {/* End Google Tag Manager (noscript) */}
+          <GtagNoscript />
           <Main />
           <NextScript />
         </body>
