c645fb7d6c684bfd05e6b27c37b0a023f9c80fec
Evan Rosa <erosa26@gmail.com>
2021-02-07 16:16:23 -0500

    contact fprm

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,106 +3,98 @@ import { useState } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Router from 'next/router';
-//import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
 
 //Original Contact Form on Contact Page
 function ContactForm() {
-  const [validated, setValidated] = useState(false);
+  const [state, setState] = useState({name: '', email: '', subject: '', message:'', sent: false, buttonText: 'Submit', err: ''} );
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
-    setValidated(true);
-  };
+ const handleChange = (e) => {
+  const {name, value} = e.target
+      setState({
+          ...state,
+          [name]: value
+  })
+};
+
+const resetForm = () => {
+  setState({
+      name: '',
+      email: '',
+      message: '',
+      subject: '',
+      sent: false,
+      buttonText: 'Submit',
+      err: ''
+  });
+};
+
+const formSubmit = (e) => {
+  e.preventDefault();
+
+  setState({
+      ...state,
+      buttonText: 'Sending...'
+  })
+
+  axios.post('/api/sendEmail', state)
+  .then(res => {
+      if(res.data.result !=='success') {
+          setState({
+              ...state,
+              buttonText: 'Failed to send',
+              sent: false,
+              err: 'fail'
+          })
+          setTimeout(() => {
+              resetForm()
+          }, 6000)        
+      } else {
+          setState({
+              ...state,
+              sent: true,
+              buttonText: 'Sent',
+              err: 'success'
+          })
+          setTimeout(() => {
+              resetForm();
+              Router.push('/thank-you')
+          }, 6000)
+      }
+  }).catch( (err) => {
+      setState({
+          ...state,
+          buttonText: 'Failed to send',
+          err: 'fail'
+      })
+  })
+};
 
   return (
     <div>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form>
         <Form.Group>
-          <Form.Control required type='text' placeholder='Name' />
+          <Form.Control required type='text' placeholder='Name' name="name" value={state.name} onChange={handleChange} />
         </Form.Group>
         <Form.Group>
-          <Form.Control required type='text' placeholder='Email' />
+          <Form.Control required type='text' placeholder='Email' name="email" value={state.email} onChange={handleChange}/>
         </Form.Group>
         <Form.Group>
-          <Form.Control required type='text' placeholder='Subject' />
+          <Form.Control required type='text' placeholder='Subject' name="subject" value={state.subject} onChange={handleChange} />
         </Form.Group>
         <Form.Group>
-          <Form.Control required type='rich-text' placeholder='Message' />
+          <Form.Control required type='rich-text' placeholder='Message' name="message" value={state.message} onChange={handleChange} />
         </Form.Group>
       </Form>
       <Button
         data-gtm-button='contact form'
         className='evro-navy-btn pt-2 pb-2 pl-4 pr-4'
-        onClick={() => Router.push('/thank-you')}
+        onClick={formSubmit}
       >
-        Send
+        {state.buttonText}
       </Button>{' '}
     </div>
   );
 }
 
 export default ContactForm;
-
-//New Contact Form
-/*
-interface emailValues {
-  name: string;
-  message: string;
-  email: string;
-}
-
-const ContactForm = () => {
-  return (
-    <div>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          message: '',
-        }}
-        onSubmit={(
-          values: emailValues,
-          { resetForm }
-        ) => {
-          setTimeout(() => {
-            //alert(JSON.stringify(values, null, 2));
-
-            fetch('/api/send-email', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ name: values.name, email: values.email, message: values.message })
-            });
-            resetForm();
-          }, 500);
-        }}
-      >
-        <Form>
-          <label htmlFor="name">Full Name</label>
-          <Field id="name" name="name" placeholder="Evan Rosa" />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
-          />
-
-          <label htmlFor="message">Message</label>
-          <Field name="message" id="message" placeholder="message to send"></Field>
-
-
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
-  );
-};
-
-export default ContactForm;
-*/
