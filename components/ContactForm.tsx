bc36abda477740b6e5305ea3a3e11814a8b521d4
Evan Rosa <erosa26@gmail.com>
2021-03-21 20:44:29 -0400

    formspree contact

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,23 +1,37 @@
-import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Router from 'next/router';
 
+
 //Original Contact Form on Contact Page
 function ContactForm() {
-  const [state, setState] = useState({name: '', email: '', subject: '', message:'', sent: false, buttonText: 'Submit', err: ''} );
-
-
-  const handleChange = (e) => {
-    const {name, value} = e.target
-        setState({
-            ...state,
-            [name]: value
-    })
-  };
-
-  const resetForm = () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  })
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const handleServerResponse = (ok, msg) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      })
+      setInputs({
+        name: '',
+        email: '',
+        message: '',
+      })
+    } 
+  }
+/*   const resetForm = () => {
     setState({
         name: '',
         email: '',
@@ -27,74 +41,71 @@ function ContactForm() {
         buttonText: 'Submit',
         err: ''
     });
-  };
-
-  const formSubmit = (e) => {
-    e.preventDefault();
-
-    setState({
-        ...state,
-        buttonText: 'Sending...'
+  }; */
+  const handleOnChange = (e) => {
+    e.persist()
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
     })
-
-    axios.post('/api/sendEmail', state)
-    .then(res => {
-        if(res.data.result !=='success') {
-            setState({
-                ...state,
-                buttonText: 'Failed to send',
-                sent: false,
-                err: 'fail'
-            })
-            setTimeout(() => {
-                resetForm()
-            }, 6000)        
-        } else {
-            setState({
-                ...state,
-                sent: true,
-                buttonText: 'Sent',
-                err: 'success'
-            })
-            setTimeout(() => {
-                resetForm();
-                Router.push('/thank-you')
-            }, 6000)
-        }
-    }).catch( (err) => {
-        setState({
-            ...state,
-            buttonText: 'Failed to send',
-            err: 'fail'
-        })
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }))
+    axios({
+      method: 'POST',
+      url: 'https://formspree.io/f/xoqyajnl',
+      data: inputs,
     })
-  };
-
+      .then((response) => {
+        handleServerResponse(
+          true,
+          'Thank you, your message has been submitted.'
+        )
+        //resetForm();
+        Router.push('/thank-you')
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error)
+      })
+  }
   return (
-    <div>
-      <Form>
+    <main>
+      <form onSubmit={handleOnSubmit}>
         <Form.Group className="pb-3">
-          <Form.Control required type='text' placeholder='Name' name="name" value={state.name} onChange={handleChange} />
+            <Form.Control required id="name" type='text' placeholder='Name' name="name" value={inputs.name} onChange={handleOnChange} />
         </Form.Group>
+
         <Form.Group className="pb-3">
-          <Form.Control required type='text' placeholder='Email' name="email" value={state.email} onChange={handleChange}/>
+            <Form.Control required id="email" type="email" placeholder='Email' name="_replyto" value={inputs.email} onChange={handleOnChange} />
         </Form.Group>
+
         <Form.Group className="pb-3">
-          <Form.Control required type='text' placeholder='Subject' name="subject" value={state.subject} onChange={handleChange} />
+          <Form.Control required id="message" type='rich-text' placeholder='Message' name="message" value={inputs.message} onChange={handleOnChange} />
         </Form.Group>
-        <Form.Group className="pb-3">
-          <Form.Control required type='rich-text' placeholder='Message' name="message" value={state.message} onChange={handleChange} />
-        </Form.Group>
-      </Form>
-      <Button
-        data-gtm-button='contact form'
-        className='evro-navy-btn pt-2 pb-2 pl-4 pr-4'
-        onClick={formSubmit}
-      >
-        {state.buttonText}
-      </Button>{' '}
-    </div>
-  );
+
+
+
+        <Button type="submit" disabled={status.submitting} data-gtm-button='contact form'         className='evro-navy-btn pt-2 pb-2 pl-4 pr-4'>
+          {!status.submitting
+            ? !status.submitted
+              ? 'Submit'
+              : 'Submitted'
+            : 'Submitting...'}
+        </Button>
+      </form>
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+    </main>
+  )
+
 }
 
 export default ContactForm;
