247c67f912008e52bbe16ab7e1f23143b8f77c5d
Evan Rosa <erosa26@gmail.com>
2021-05-22 13:23:04 -0400

    npm audit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins([
-  withCSS({
-    cssLoaderOptions: {
-      url: false,
-    },
-  }),
-  withImages,
+  withImages
 ]);
